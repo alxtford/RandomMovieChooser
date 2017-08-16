f285c6ca3c25d8ef19ed546f4aa1396b21b2d932
Toby <tobyreed0@gmail.com>
2017-08-16 14:02:08 +0100

    Added Random Film Picking Capabilities
    Added the ability to pick a random film and rechoose if the user is
    unhappy with the choice.

--- RandomMovieChooser/Program.cs
+++ RandomMovieChooser/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RandomMovieChooser
 {
@@ -10,6 +8,61 @@ namespace RandomMovieChooser
     {
         static void Main(string[] args)
         {
+            randomMovieChooser();
+
+            chooseAgain();
+        }
+
+        static void randomMovieChooser()
+        {
+            Random RNG = new Random();
+
+            string[] file;
+
+            file = File.ReadAllLines(Environment.CurrentDirectory + @"\Film List.txt");
+
+            int filmID = 0;
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                filmID = i;
+            }
+
+            Console.WriteLine(file[RNG.Next(1, file.Length)]);
+
+        }
+
+        static void closeProgram()
+        {
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    //Console.WriteLine("Waiting");
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            {
+                randomMovieChooser();
+            }
+        }
+        static void chooseAgain()
+        {
+            Console.WriteLine("If you'd like to choose again, press ENTER");
+
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    //Console.WriteLine("Waiting");
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+            {
+                randomMovieChooser();
+            }
+
+            chooseAgain();
         }
     }
 }
